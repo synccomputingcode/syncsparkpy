0fe2133533da82993eeebfd0ab04f53a914a3f2e
Scott <scott@synccomputing.com>
2023-03-20 16:21:39 -0700

    Prod 738 demo (#16)

--- demo/install_cli.sh
+++ demo/install_cli.sh
@@ -1,33 +1,42 @@
 # Source this script to install sync-cli in a virtual environment at ../venv
 
 
-HOME_DIR="$(dirname "$(dirname "$(readlink -f $0)")")"
+SYNC_HOME="$(dirname "$(dirname "$(readlink -f $0)")")"
 
-if ! [[ $(python --version 2>/dev/null) =~ 3.10 ]]; then
-  >&2 echo "Python 3.10 is required"
-  return 1
-fi
+{
+  local SYNC_PYTHON="$(command -v python3 python | sed q)"
 
-if [[ -z $VIRTUAL_ENV ]]; then
-  if ! { python -m venv "$HOME_DIR/venv" && source "$HOME_DIR/venv/bin/activate"; }; then
-    >&2 echo "Failed to activate virtual environment"
+  if ! [[ $("$SYNC_PYTHON" --version 2>/dev/null) =~ '(^|[^[:digit:].])3.10(\.|$)' ]]; then
+    >&2 echo "Python 3.10 is required"
     return 1
   fi
-fi
 
-pip install -e $HOME_DIR
+  if [[ -z $VIRTUAL_ENV ]]; then
+    if ! { "$SYNC_PYTHON" -m venv "$SYNC_HOME/venv" && source "$SYNC_HOME/venv/bin/activate"; }; then
+      >&2 echo "Failed to activate virtual environment"
+      return 1
+    fi
+  fi
 
-cat <<"EOD"
-                                     ___
-   _______  ______  _____      _____/ (_)
-  / ___/ / / / __ \/ ___/_____/ ___/ / /
- (__  ) /_/ / / / / /__/_____/ /__/ / /
-/____/\__, /_/ /_/\___/      \___/_/_/
-     /____/
+  local SYNC_PIP="$(command -v pip3 pip | sed q)"
+
+  if "$SYNC_PIP" install -e $SYNC_HOME; then
+    cat <<"EOD"
+                                         ___
+       _______  ______  _____      _____/ (_)
+      / ___/ / / / __ \/ ___/_____/ ___/ / /
+     (__  ) /_/ / / / / /__/_____/ /__/ / /
+    /____/\__, /_/ /_/\___/      \___/_/_/
+         /____/
 
 
-sync-cli successfully installed!
+    Installation successful!
 
-Try `sync-cli --help` to see available commands and options.
+    Try `sync-cli --help` to see available commands and options.
 
 EOD
+  else
+    >&2 echo "Failed to install Sync CLI"
+    return 1
+  fi
+}

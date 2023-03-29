4c8526a558550225eb74cf227336199d5aabc77f
Scott Romney <scott@synccomputing.com>
2023-03-29 13:05:52 -0700

    Wrap up 'create', 'status' and 'get' commands into 'generate'

--- sync/cli/predictions.py
+++ sync/cli/predictions.py
@@ -13,6 +13,7 @@ from sync.api.predictions import (
     get_predictions,
     get_products,
     get_status,
+    wait_for_prediction,
 )
 from sync.cli.util import validate_project
 from sync.config import CONFIG
@@ -33,6 +34,71 @@ def platforms():
         click.echo(", ".join(product for product in products if product != "aws-databricks"))
     else:
         click.echo(str(products_response.error), err=True)
+
+
+@predictions.command
+@click.argument(
+    "platform",
+    type=click.Choice(platform for platform in Platform if platform is not Platform.AWS_DATABRICKS),
+)
+@click.option("-e", "--event-log", metavar="URL/PATH", required=True)
+@click.option("-c", "--config", metavar="URL/PATH", required=True)
+@click.option("--project", callback=validate_project, help="project/app ID")
+@click.option(
+    "--preference",
+    type=click.Choice(Preference),
+    default=CONFIG.default_prediction_preference,
+)
+def generate(platform: Platform, event_log: str, config: str, project: str, preference: Preference):
+    """Create and retrieve a prediction"""
+    parsed_config = urlparse(config)
+    match parsed_config.scheme:
+        case "":
+            with open(config) as config_fobj:
+                config = orjson.loads(config_fobj.read())
+        case "s3":
+            s3 = boto.client("s3")
+            config_io = io.BytesIO()
+            s3.download_fileobj(parsed_config.netloc, parsed_config.path.lstrip("/"), config_io)
+            config = orjson.loads(config_io.getvalue())
+        case _:
+            click.echo("Unsupported config argument", err=True)
+
+    parsed_event_log_loc = urlparse(event_log)
+    event_log_path = None
+    event_log_url = None
+    match parsed_event_log_loc.scheme:
+        case "":
+            event_log_path = Path(event_log)
+        case "s3" | "http" | "https":
+            event_log_url = event_log
+        case _:
+            click.echo("Unsupported config argument", err=True)
+
+    if event_log_url:
+        response = create_prediction(platform, config, event_log_url, project["id"])
+    elif event_log_path:
+        with open(event_log_path, "rb") as event_log_fobj:
+            response = create_prediction_with_eventlog_bytes(
+                platform, config, event_log_path.name, event_log_fobj.read(), project["id"]
+            )
+
+    if prediction_id := response.result:
+        prediction_response = wait_for_prediction(prediction_id, preference.value)
+        if prediction := prediction_response.result:
+            click.echo(
+                orjson.dumps(
+                    prediction,
+                    option=orjson.OPT_INDENT_2
+                    | orjson.OPT_UTC_Z
+                    | orjson.OPT_NAIVE_UTC
+                    | orjson.OPT_OMIT_MICROSECONDS,
+                )
+            )
+        else:
+            click.echo(str(response.error), err=True)
+    else:
+        click.echo(str(response.error), err=True)
 
 
 @predictions.command

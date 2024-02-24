ce331a1eb5f0e96a65ffd88cfdcde193bd180f06
Sahil Singhal <sahil.singhal@synccomputing.com>
2024-02-24 11:57:55 -0500

    Fix mocking for config tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,42 +1,44 @@
 import unittest
 import pytest
-
+from unittest.mock import patch
 from sync import config
 
 
 class TestConfig(unittest.TestCase):
-    def test_set_api_key_twice_raises_an_error(
-        self
+    @patch("sync.config._api_key")
+    def test_set_api_key_when_api_key_exists_raises_an_error(
+        self,
+        mock_api_key
     ):
-        mock_api_key = config.APIKey(
+        mock_api_key.return_value = config.APIKey(
             api_key_id="mock_api_key_id",
             api_key_secret="mock_api_key_secret"
         )
-        config.set_api_key(mock_api_key)
 
-        mock_api_key_2 = config.APIKey(
+        new_api_key = config.APIKey(
             api_key_id="mock_api_key_id_2",
             api_key_secret="mock_api_key_secret_2"
         )
 
         with pytest.raises(RuntimeError):
-            config.set_api_key(mock_api_key_2)
+            config.set_api_key(new_api_key)
 
-    def test_set_databricks_config_twice_raises_an_error(
-        self
+    @patch("sync.config._db_config")
+    def test_set_databricks_config_when_db_config_exists_raises_an_error(
+        self,
+        mock_db_config
     ):
-        mock_dbx_config = config.DatabricksConf(
+        mock_db_config.return_value = config.DatabricksConf(
             host="https://fakedbx-host.com",
             token="dsapifaketoken",
             aws_region_name="us-east-1"
         )
-        config.set_databricks_config(mock_dbx_config)
 
-        mock_dbx_config_2 = config.DatabricksConf(
+        new_db_config = config.DatabricksConf(
             host="https://fakedbx-host-2.com",
             token="dsapifaketoken2",
             aws_region_name="us-east-2"
         )
 
         with pytest.raises(RuntimeError):
-            config.set_databricks_config(mock_dbx_config_2)
+            config.set_databricks_config(new_db_config)

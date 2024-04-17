3ea126718b64f433e146cc5f85929e03fa2d4e36
Brandon Kaplan <Bkaplan31@gmail.com>
2024-04-17 16:11:31 -0500

    Add ability to Create and Update workspaces

--- sync/cli/workspaces.py
+++ sync/cli/workspaces.py
@@ -1,11 +1,22 @@
 import json
+from urllib.parse import urlparse
+from uuid import uuid4
 
 import click
 
 from sync.api import workspace
 from sync.cli.util import OPTIONAL_DEFAULT, validate_project
 from sync.config import API_KEY, DB_CONFIG
-from sync.models import DatabricksPlanType
+from sync.models import (
+    AwsHostedIAMInstructions,
+    AwsRegionEnum,
+    ComputeProvider,
+    CreateWorkspaceConfig,
+    DatabricksPlanType,
+    HostingType,
+    UpdateWorkspaceConfig,
+    WorkspaceCollectionTypeEnum,
+)
 from sync.utils.json import DateTimeEncoderNaiveUTCDropMicroseconds
 
 
@@ -32,11 +43,42 @@ def workspaces():
 )
 @click.option(
     "--aws-region",
+    type=click.Choice(AwsRegionEnum),
     help="Workspace region",
 )
 @click.option(
     "--cluster-policy-id",
     help="ID of cluster policy to apply to Sync job reporting cluster",
+)
+@click.option(
+    "--hosting-type",
+    type=click.Choice(HostingType),
+    help="Indicates where the workspace is hosted",
+)
+@click.option(
+    "--compute-provider",
+    type=click.Choice(ComputeProvider),
+    help="Indicates the compute provider for the workspace",
+)
+@click.option(
+    "--aws-iam-role-arn",
+    help="AWS IAM Role ARN",
+)
+@click.option(
+    "--azure-subscription-id",
+    help="Azure Subscription ID",
+)
+@click.option(
+    "--azure-client-id",
+    help="Azure Client ID",
+)
+@click.option(
+    "--azure-tenant-id",
+    help="Azure Tenant ID",
+)
+@click.option(
+    "--azure-client-secret",
+    help="Azure Client Secret",
 )
 def create_workspace_config(
     workspace_id: str,
@@ -45,41 +87,158 @@ def create_workspace_config(
     databricks_webhook_id: str = None,
     aws_region: str = None,
     cluster_policy_id: str = None,
+    hosting_type: HostingType = None,
+    compute_provider: ComputeProvider = None,
+    aws_iam_role_arn: str = None,
+    azure_subscription_id: str = None,
+    azure_client_id: str = None,
+    azure_tenant_id: str = None,
+    azure_client_secret: str = None,
 ):
-    databricks_host = click.prompt(
-        "Databricks host (prefix with https://)", default=DB_CONFIG.host if DB_CONFIG else None
-    )
-    databricks_token = click.prompt(
-        "Databricks token",
-        default=DB_CONFIG.token if DB_CONFIG else None,
-        hide_input=True,
-        show_default=False,
-    )
-    sync_api_key_id = click.prompt("Sync API key ID", default=API_KEY.id if API_KEY else None)
-    sync_api_key_secret = click.prompt(
-        "Sync API key secret",
-        default=API_KEY.secret if API_KEY else None,
-        hide_input=True,
-        show_default=False,
+    databricks_host, databricks_token = _prompt_databricks_fields()
+
+    sync_api_key_id, sync_api_key_secret = _prompt_sync_api_fields()
+
+    compute_provider: ComputeProvider = _determine_compute_provider(
+        compute_provider, databricks_host
     )
 
-    response = workspace.create_workspace_config(
-        workspace_id,
+    aws_region = (
+        aws_region if aws_region is None and compute_provider == ComputeProvider.AWS else None
+    )
+
+    hosting_type = _prompt_hosting_type(hosting_type)
+    collection_type = _determine_collection_type(hosting_type)
+    aws_external_id = str(uuid4())
+
+    if hosting_type == HostingType.SELF_HOSTED and compute_provider == ComputeProvider.AWS:
+        instance_profile_arn = instance_profile_arn or click.prompt("AWS Instance profile ARN")
+
+    if hosting_type == HostingType.SYNC_HOSTED:
+        if compute_provider == ComputeProvider.AWS:
+            aws_iam_role_arn = _prompt_aws_arn_info(aws_external_id, aws_iam_role_arn)
+        elif compute_provider == ComputeProvider.AZURE:
+            (
+                azure_client_id,
+                azure_client_secret,
+                azure_subscription_id,
+                azure_tenant_id,
+            ) = _handle_azure_provider(
+                azure_client_id, azure_client_secret, azure_subscription_id, azure_tenant_id
+            )
+
+    workspace_config = CreateWorkspaceConfig(
+        workspace_id=workspace_id,
         databricks_host=databricks_host,
         databricks_token=databricks_token,
         sync_api_key_id=sync_api_key_id,
         sync_api_key_secret=sync_api_key_secret,
+        collection_type=collection_type,
+        compute_provider=compute_provider,
         instance_profile_arn=instance_profile_arn,
-        webhook_id=databricks_webhook_id,
         databricks_plan_type=databricks_plan_type,
+        webhook_id=databricks_webhook_id,
         aws_region=aws_region,
+        aws_iam_role_arn=aws_iam_role_arn,
         cluster_policy_id=cluster_policy_id,
+        external_id=aws_external_id,
+        azure_tenant_id=azure_tenant_id,
+        azure_client_id=azure_client_id,
+        azure_client_secret=azure_client_secret,
+        azure_subscription_id=azure_subscription_id,
     )
+    response = workspace.create_workspace_config(workspace_config)
+
     config = response.result
     if config:
         click.echo(json.dumps(config, indent=2, cls=DateTimeEncoderNaiveUTCDropMicroseconds))
     else:
         click.echo(str(response.error), err=True)
+
+
+def _prompt_aws_arn_info(aws_external_id, aws_iam_role_arn):
+    instructions = AwsHostedIAMInstructions(external_id=aws_external_id)
+    click.echo(click.style(text=instructions.step_1_prompt, bold=True))
+    click.echo(instructions.step_1_value)
+    click.echo(click.style(text=instructions.step_2_prompt, bold=True))
+    click.echo(instructions.step_2_value)
+    aws_iam_role_arn = aws_iam_role_arn or click.prompt(
+        "AWS IAM Role ARN",
+    )
+    return aws_iam_role_arn
+
+
+def _prompt_hosting_type(hosting_type):
+    hosting_type = hosting_type or click.prompt(
+        "Hosting type (sync-hosted or self-hosted)",
+        type=click.Choice([h.value for h in HostingType]),
+        default=HostingType.SYNC_HOSTED,
+        show_default=True,
+    )
+    return hosting_type
+
+
+def _prompt_sync_api_fields():
+    sync_api_key_id = click.prompt("Sync API key ID", default=getattr(API_KEY, "id", None))
+    sync_api_key_secret = click.prompt(
+        "Sync API key secret",
+        default=getattr(API_KEY, "secret", None),
+        hide_input=True,
+        show_default=False,
+    )
+    return sync_api_key_id, sync_api_key_secret
+
+
+def _prompt_databricks_fields():
+    databricks_host = click.prompt(
+        "Databricks host (prefix with https://)", default=getattr(DB_CONFIG, "host", None)
+    )
+    databricks_token = click.prompt(
+        "Databricks token",
+        default=getattr(DB_CONFIG, "token", None),
+        hide_input=True,
+        show_default=False,
+    )
+    return databricks_host, databricks_token
+
+
+def _determine_collection_type(hosting_type):
+    return (
+        WorkspaceCollectionTypeEnum.HOSTED
+        if hosting_type == HostingType.SYNC_HOSTED
+        else WorkspaceCollectionTypeEnum.REMOTE
+    )
+
+
+def _handle_azure_provider(
+    azure_client_id, azure_client_secret, azure_subscription_id, azure_tenant_id
+):
+    azure_subscription_id = azure_subscription_id or click.prompt("Azure Subscription ID")
+    azure_client_id = azure_client_id or click.prompt("Azure Client ID")
+    azure_tenant_id = azure_tenant_id or click.prompt("Azure Tenant ID")
+    azure_client_secret = azure_client_secret or click.prompt("Azure Client Secret")
+    return azure_client_id, azure_client_secret, azure_subscription_id, azure_tenant_id
+
+
+def _get_provider_from_url(url: str) -> str:
+    netloc = urlparse(url).netloc
+    if netloc.endswith(".azuredatabricks.net"):
+        return ComputeProvider.AZURE
+    elif netloc.endswith(".cloud.databricks.com"):
+        return ComputeProvider.AWS
+
+
+def _determine_compute_provider(compute_provider, databricks_host):
+    return (
+        compute_provider
+        or _get_provider_from_url(databricks_host)
+        or click.prompt(
+            "Compute provider (aws or azure)",
+            type=click.Choice(ComputeProvider),
+            default=ComputeProvider.AWS,
+            show_default=True,
+        )
+    )
 
 
 @workspaces.command
@@ -128,6 +287,26 @@ def list_workspace_configs():
     "--cluster-policy-id",
     help="ID of cluster policy to apply to Sync job reporting cluster",
 )
+@click.option(
+    "--aws-iam-role-arn",
+    help="AWS IAM Role ARN",
+)
+@click.option(
+    "--azure-subscription-id",
+    help="Azure Subscription ID",
+)
+@click.option(
+    "--azure-client-id",
+    help="Azure Client ID",
+)
+@click.option(
+    "--azure-tenant-id",
+    help="Azure Tenant ID",
+)
+@click.option(
+    "--azure-client-secret",
+    help="Azure Client Secret",
+)
 def update_workspace_config(
     workspace_id: str,
     instance_profile_arn: str = None,
@@ -135,6 +314,11 @@ def update_workspace_config(
     databricks_webhook_id: str = None,
     aws_region: str = None,
     cluster_policy_id: str = None,
+    aws_iam_role_arn: str = None,
+    azure_subscription_id: str = None,
+    azure_client_id: str = None,
+    azure_tenant_id: str = None,
+    azure_client_secret: str = None,
 ):
     current_config_response = workspace.get_workspace_config(workspace_id)
     current_config = current_config_response.result
@@ -153,21 +337,27 @@ def update_workspace_config(
         sync_api_key_secret = click.prompt(
             "Sync API key secret (optional)", default=OPTIONAL_DEFAULT, show_default=False
         )
-
-        update_config_response = workspace.update_workspace_config(
-            workspace_id,
+        update_configuration = UpdateWorkspaceConfig(
+            workspace_id=workspace_id,
             databricks_host=databricks_host if databricks_host != OPTIONAL_DEFAULT else None,
             databricks_token=databricks_token if databricks_token != OPTIONAL_DEFAULT else None,
             sync_api_key_id=sync_api_key_id if sync_api_key_id != OPTIONAL_DEFAULT else None,
-            sync_api_key_secret=sync_api_key_secret
-            if sync_api_key_secret != OPTIONAL_DEFAULT
-            else None,
+            sync_api_key_secret=(
+                sync_api_key_secret if sync_api_key_secret != OPTIONAL_DEFAULT else None
+            ),
             instance_profile_arn=instance_profile_arn,
             webhook_id=databricks_webhook_id,
             databricks_plan_type=databricks_plan_type,
             aws_region=aws_region,
             cluster_policy_id=cluster_policy_id,
+            aws_iam_role_arn=aws_iam_role_arn,
+            azure_subscription_id=azure_subscription_id,
+            azure_client_id=azure_client_id,
+            azure_tenant_id=azure_tenant_id,
+            azure_client_secret=azure_client_secret,
         )
+
+        update_config_response = workspace.update_workspace_config(update_configuration)
         config = update_config_response.result
         if config:
             click.echo(
--- sync/models.py
+++ sync/models.py
@@ -4,7 +4,7 @@ Models used throughout this SDK
 
 from dataclasses import dataclass
 from enum import Enum, unique
-from typing import Callable, Dict, Generic, List, TypeVar, Union
+from typing import Callable, Dict, Generic, List, Optional, TypeVar, Union
 
 from botocore.exceptions import ClientError
 from pydantic import BaseModel, Field, root_validator, validator
@@ -14,6 +14,24 @@ from pydantic.generics import GenericModel
 class Platform(str, Enum):
     AWS_DATABRICKS = "aws-databricks"
     AZURE_DATABRICKS = "azure-databricks"
+
+
+@unique
+class HostingType(str, Enum):
+    SYNC_HOSTED = "sync-hosted"
+    SELF_HOSTED = "self-hosted"
+
+
+@unique
+class WorkspaceCollectionTypeEnum(str, Enum):
+    REMOTE = "remote"
+    HOSTED = "hosted"
+
+
+@unique
+class ComputeProvider(str, Enum):
+    AWS = "aws"
+    AZURE = "azure"
 
 
 class AccessStatusCode(str, Enum):
@@ -175,3 +193,182 @@ class AzureProjectConfiguration(BaseModel):
     spark_version: str
     runtime_engine: str
     azure_attributes: Dict
+
+
+class AwsRegionEnum(str, Enum):
+    US_EAST_1 = "us-east-1"
+    US_EAST_2 = "us-east-2"
+    US_WEST_1 = "us-west-1"
+    US_WEST_2 = "us-west-2"
+    AF_SOUTH_1 = "af-south-1"
+    AP_EAST_1 = "ap-east-1"
+    AP_SOUTH_1 = "ap-south-1"
+    AP_SOUTH_2 = "ap-south-2"
+    AP_NORTHEAST_1 = "ap-northeast-1"
+    AP_NORTHEAST_2 = "ap-northeast-2"
+    AP_NORTHEAST_3 = "ap-northeast-3"
+    AP_SOUTHEAST_1 = "ap-southeast-1"
+    AP_SOUTHEAST_2 = "ap-southeast-2"
+    AP_SOUTHEAST_3 = "ap-southeast-3"
+    AP_SOUTHEAST_4 = "ap-southeast-4"
+    CA_CENTRAL_1 = "ca-central-1"
+    CA_WEST_1 = "ca-west-1"
+    EU_CENTRAL_1 = "eu-central-1"
+    EU_CENTRAL_2 = "eu-central-2"
+    EU_WEST_1 = "eu-west-1"
+    EU_WEST_2 = "eu-west-2"
+    EU_WEST_3 = "eu-west-3"
+    EU_SOUTH_1 = "eu-south-1"
+    EU_SOUTH_2 = "eu-south-2"
+    EU_NORTH_1 = "eu-north-1"
+    IL_CENTRAL_1 = "il-central-1"
+    ME_SOUTH_1 = "me-south-1"
+    ME_CENTRAL_1 = "me-central-1"
+    SA_EAST_1 = "sa-east-1"
+
+
+class AwsHostedIAMInstructions(BaseModel):
+    step_1_prompt: str = "Step 1: Copy the JSON and paste in AWS IAM Permissions page:"
+    step_1_value: str = """
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "VisualEditor0",
+                "Effect": "Allow",
+                "Action": [
+                    "ec2:DescribeInstances",
+                    "ec2:DescribeVolumes"
+                ],
+                "Resource": "*"
+            }
+        ]
+    }"""
+    step_2_prompt: str = (
+        "Step 2: Copy the JSON and paste in AWS IAM Trust relationships page with External ID:"
+    )
+    external_id: str
+
+    @property
+    def step_2_value(self) -> str:
+        return f"""
+        {{
+            "Version": "2012-10-17",
+            "Statement": [
+                {{
+                    "Effect": "Allow",
+                    "Principal": {{
+                        "AWS": "arn:aws:iam::533267411813:role/sync-computing-collector"
+                    }},
+                    "Action": "sts:AssumeRole",
+                    "Condition": {{
+                        "StringEquals": {{
+                            "sts:ExternalId": "{self.external_id}"
+                        }}
+                    }}
+                }}
+            ]
+        }}"""
+
+
+class ComputeProviderHostedValues(BaseModel):
+    aws_iam_role_arn: Optional[str]
+    azure_subscription_id: Optional[str]
+    azure_tenant_id: Optional[str]
+    azure_client_id: Optional[str]
+    azure_client_secret: Optional[str]
+
+
+class CreateWorkspaceConfig(BaseModel):
+    workspace_id: str = Field(..., description="Unique identifier for the workspace")
+    databricks_host: str = Field(..., description="Databricks service host URL")
+    databricks_token: str = Field(..., description="Authentication token for Databricks service")
+    sync_api_key_id: str = Field(..., description="API Key ID for synchronization service")
+    sync_api_key_secret: str = Field(..., description="API Key secret for synchronization service")
+    instance_profile_arn: Optional[str] = Field(None, description="AWS instance profile ARN")
+    webhook_id: Optional[str] = Field(None, description="Webhook ID for notifications")
+    databricks_plan_type: DatabricksPlanType = Field(
+        DatabricksPlanType.STANDARD, description="Plan type for Databricks deployment"
+    )
+    aws_region: Optional[str] = Field(None, description="AWS region if applicable")
+    cluster_policy_id: Optional[str] = Field(None, description="Cluster policy ID for Databricks")
+    collection_type: WorkspaceCollectionTypeEnum = Field(
+        ..., description="Type of hosting for the workspace"
+    )
+    compute_provider: ComputeProvider = Field(
+        ..., description="Cloud provider for compute resources"
+    )
+    external_id: Optional[str] = Field(None, description="External ID for AWS configurations")
+    aws_iam_role_arn: Optional[str] = Field(None, description="AWS IAM role ARN if needed")
+    azure_tenant_id: Optional[str] = Field(None, description="Azure tenant ID if using Azure")
+    azure_client_id: Optional[str] = Field(None, description="Azure client ID if using Azure")
+    azure_client_secret: Optional[str] = Field(
+        None, description="Azure client secret if using Azure"
+    )
+    azure_subscription_id: Optional[str] = Field(
+        None, description="Azure subscription ID if using Azure"
+    )
+
+    @validator("aws_region")
+    def check_aws_region(cls, region, values):
+        if values.get("compute_provider") == ComputeProvider.AWS and not region:
+            raise ValueError("AWS Region is required for AWS compute provider")
+        return region
+
+    @validator("instance_profile_arn")
+    def ensure_instance_profile_arn(cls, arn, values):
+        if (
+            values.get("collection_type") == WorkspaceCollectionTypeEnum.REMOTE
+            and values.get("compute_provider") == ComputeProvider.AWS
+            and not arn
+        ):
+            raise ValueError(
+                "AWS Instance Profile ARN is required for REMOTE hosted AWS environments"
+            )
+        return arn
+
+    @validator("aws_iam_role_arn", pre=False)
+    def check_aws_iam_role_arn(cls, aws_iam_role_arn, values):
+        compute_provider = values.get("compute_provider")
+        if values.get("collection_type") == WorkspaceCollectionTypeEnum.HOSTED:
+            if compute_provider == ComputeProvider.AWS and not aws_iam_role_arn:
+                raise ValueError("AWS IAM Role ARN is required for AWS compute provider")
+        return aws_iam_role_arn
+
+    @validator("compute_provider", pre=False)
+    def check_azure_hosted_fields(cls, compute_provider, values):
+        if (
+            values.get("compute_provider") == ComputeProvider.AZURE
+            and values.get("collection_type") == WorkspaceCollectionTypeEnum.HOSTED
+        ):
+            required_fields = [
+                "azure_tenant_id",
+                "azure_client_id",
+                "azure_client_secret",
+                "azure_subscription_id",
+            ]
+            missing_fields = [field for field in required_fields if not values.get(field)]
+            if missing_fields:
+                raise ValueError(
+                    f"Missing required fields for Azure compute provider: "
+                    f"{', '.join(missing_fields)}"
+                )
+        return compute_provider
+
+
+class UpdateWorkspaceConfig(BaseModel):
+    workspace_id: str
+    databricks_host: Optional[str] = None
+    databricks_token: Optional[str] = None
+    sync_api_key_id: Optional[str] = None
+    sync_api_key_secret: Optional[str] = None
+    instance_profile_arn: Optional[str] = None
+    databricks_plan_type: Optional[str] = None
+    webhook_id: Optional[str] = None
+    aws_region: Optional[str] = None
+    cluster_policy_id: Optional[str] = None
+    aws_iam_role_arn: Optional[str] = None
+    azure_subscription_id: Optional[str] = None
+    azure_tenant_id: Optional[str] = None
+    azure_client_id: Optional[str] = None
+    azure_client_secret: Optional[str] = None

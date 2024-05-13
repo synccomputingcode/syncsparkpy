1c880520b3b05cf6c2737e93aec12e3e048fcfe4
Sahil Singhal <sahil.singhal@synccomputing.com>
2024-05-13 15:31:11 -0400

    Support multiple tasks in the operator def

--- sync/__init__.py
+++ sync/__init__.py
@@ -1,5 +1,5 @@
 """Library for leveraging the power of Sync"""
 
-__version__ = "1.7.0-alpha"
+__version__ = "1.7.1-alpha"
 
 TIME_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
--- sync/databricks/integrations/_run_submit_runner.py
+++ sync/databricks/integrations/_run_submit_runner.py
@@ -2,55 +2,76 @@ import logging
 
 from sync import _databricks as databricks
 from sync.api import projects
-from sync.models import Platform
 from sync.api import workspace
-
+from sync.models import Platform
 
 logger = logging.getLogger(__name__)
 
 compute_provider_platform_dict = {
     "aws": Platform.AWS_DATABRICKS,
-    "azure": Platform.AZURE_DATABRICKS
+    "azure": Platform.AZURE_DATABRICKS,
 }
 
 
 def apply_sync_gradient_cluster_recommendation(
-    run_submit_task: dict,
-    gradient_app_id: str,
-    auto_apply: bool,
-    cluster_log_url: str,
-    workspace_id: str
+        run_submit_task: dict,
+        gradient_app_id: str,
+        auto_apply: bool,
+        cluster_log_url: str,
+        workspace_id: str,
 ) -> dict:
+    if tasks := run_submit_task.get("tasks"):
+        if len(tasks) > 1:
+            logger.error(
+                "Unable to apply gradient configuration for run submit " "with multiple tasks."
+            )
+        original_cluster = tasks[0]["new_cluster"]
+    else:
+        original_cluster = run_submit_task["new_cluster"]
+
     response = workspace.get_workspace_config(workspace_id)
     workspace_config = response.result
 
-    project_id = create_or_fetch_project(gradient_app_id, cluster_log_url, workspace_config.get("compute_provider"))
+    project_id = create_or_fetch_project(
+        gradient_app_id, cluster_log_url, workspace_config.get("compute_provider")
+    )
 
     # if recommendation exists apply the configuration or apply default configuration with project settings
     if auto_apply:
-        logger.info(f"Generating recommendation - app_id: {gradient_app_id}, project_id: {project_id}, "
-                    f"auto_apply: {auto_apply}")
+        logger.info(
+            f"Generating recommendation - app_id: {gradient_app_id}, project_id: {project_id}, "
+            f"auto_apply: {auto_apply}"
+        )
         rec = get_gradient_recommendation(project_id)
         if rec:
-            logger.info(f"Recommendation generated - app_id: {gradient_app_id}, project_id: {project_id}, "
-                        f"recommendation: {rec}")
+            logger.info(
+                f"Recommendation generated - app_id: {gradient_app_id}, project_id: {project_id}, "
+                f"recommendation: {rec}"
+            )
             updated_cluster = rec
         else:
-            logger.warning(f"Unable to generate recommendation. Falling back to original cluster - "
-                           f"app_id: {gradient_app_id}, project_id: {project_id}, "
-                           f"auto_apply: {auto_apply}, cluster: {run_submit_task['new_cluster']}")
-            updated_cluster = run_submit_task["new_cluster"]
+            logger.warning(
+                f"Unable to generate recommendation. Falling back to original cluster - "
+                f"app_id: {gradient_app_id}, project_id: {project_id}, "
+                f"auto_apply: {auto_apply}, cluster: {original_cluster}"
+            )
+            updated_cluster = original_cluster
     else:
-        updated_cluster = run_submit_task["new_cluster"]
+        updated_cluster = original_cluster
 
     resp = databricks.get_project_cluster(updated_cluster, project_id)
     if resp.result:
         configured_cluster = resp.result
-        run_submit_task["new_cluster"] = configured_cluster
+        if run_submit_task.get("tasks"):
+            run_submit_task["tasks"][0]["new_cluster"] = configured_cluster
+        else:
+            run_submit_task["new_cluster"] = configured_cluster
         run_submit_task = apply_webhook_notification(workspace_config, run_submit_task)
     else:
-        logger.error("Unable to apply gradient configuration to databricks run submit call. "
-                     "Submitting original run submit call.")
+        logger.error(
+            "Unable to apply gradient configuration to databricks run submit call. "
+            "Submitting original run submit call."
+        )
 
     return run_submit_task
 
@@ -58,18 +79,21 @@ def apply_sync_gradient_cluster_recommendation(
 def create_or_fetch_project(app_id: str, cluster_log_url: str, compute_provider: str) -> str:
     resp = projects.get_project_by_app_id(app_id)
     if resp.result is None:
-        logger.info(f"Project with app_id does not exist. Creating project - app_id:{app_id}, "
-                    f"cluster_log_url:{cluster_log_url}, compute_provider:{compute_provider}")
+        logger.info(
+            f"Project with app_id does not exist. Creating project - app_id:{app_id}, "
+            f"cluster_log_url:{cluster_log_url}, compute_provider:{compute_provider}"
+        )
         resp = projects.create_project(
             name=app_id,
             app_id=app_id,
             cluster_log_url=cluster_log_url,
-            product_code=compute_provider_platform_dict.get(compute_provider) or Platform.AWS_DATABRICKS
+            product_code=compute_provider_platform_dict.get(compute_provider)
+                         or Platform.AWS_DATABRICKS,
         )
     else:
         logger.info(f"Found project with app_id - app_id:{app_id}")
 
-    return resp.result['id']
+    return resp.result["id"]
 
 
 def get_gradient_recommendation(project_id: str) -> dict:
@@ -88,7 +112,7 @@ def get_gradient_recommendation(project_id: str) -> dict:
 
     response = projects.wait_for_recommendation(project_id, recommendation_id)
 
-    return response.result['recommendation']['configuration']
+    return response.result["recommendation"]["configuration"]
 
 
 def apply_webhook_notification(workspace_config: dict, task: dict) -> dict:
@@ -101,9 +125,7 @@ def apply_webhook_notification(workspace_config: dict, task: dict) -> dict:
 
 
 def append_webhook(task: dict, webhook_id: str, event: str) -> dict:
-    webhook_request = {
-        "id": webhook_id
-    }
+    webhook_request = {"id": webhook_id}
     task["webhook_notifications"] = task.get("webhook_notifications") or {}
     task["webhook_notifications"][event] = task["webhook_notifications"].get(event) or []
     task["webhook_notifications"][event].append(webhook_request)

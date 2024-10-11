6a3ad1d2cbebc01bbdd4dd1ab5ec9223d6fc359a
Samantha Hughes <shughes.uk@gmail.com>
2024-10-11 09:57:09 -0600

    integration test compat

--- sync/cli/workspaces.py
+++ sync/cli/workspaces.py
@@ -7,7 +7,7 @@ import click
 
 from sync.api import workspace
 from sync.cli.util import OPTIONAL_DEFAULT, validate_project
-from sync.config import API_KEY, DB_CONFIG
+from sync.config import API_KEY
 from sync.models import (
     AwsHostedIAMInstructions,
     AwsRegionEnum,
@@ -36,7 +36,7 @@ def workspaces():
 )
 @click.option(
     "--databricks-plan-type",
-    type=click.Choice(DatabricksPlanType),
+    type=click.Choice(list(DatabricksPlanType)),
     default=DatabricksPlanType.STANDARD,
 )
 @click.option(
@@ -45,7 +45,7 @@ def workspaces():
 )
 @click.option(
     "--aws-region",
-    type=click.Choice(AwsRegionEnum),
+    type=click.Choice(list(AwsRegionEnum)),
     help="Workspace region",
 )
 @click.option(
@@ -54,12 +54,14 @@ def workspaces():
 )
 @click.option(
     "--hosting-type",
-    type=click.Choice(HostingType),
+    type=click.Choice(list(HostingType)),
     help="Indicates where the workspace is hosted",
+    prompt=True,
+    default=lambda: HostingType.SYNC_HOSTED,
 )
 @click.option(
     "--compute-provider",
-    type=click.Choice(ComputeProvider),
+    type=click.Choice([v for v in ComputeProvider]),
     help="Indicates the compute provider for the workspace",
 )
 @click.option(
@@ -84,51 +86,80 @@ def workspaces():
 )
 @click.option(
     "--monitoring-type",
-    type=click.Choice(WorkspaceMonitoringTypeEnum),
+    type=click.Choice(list(WorkspaceMonitoringTypeEnum)),
     default=WorkspaceMonitoringTypeEnum.WEBHOOK,
     help=(
         "Choose how you want Gradient to monitor your Databricks clusters. "
         "Ignored for REMOTE collection type."
     ),
 )
+@click.option(
+    "--databricks-host", help="Databricks host (prefix with https://)", prompt=True
+)
+@click.option(
+    "--databricks-token", help="Databricks token", prompt=True, hide_input=True
+)
+@click.option(
+    "--sync-api-key-id",
+    help="Sync API key ID",
+    prompt=True,
+    default=getattr(API_KEY, "id", None),
+)
+@click.option(
+    "--sync-api-key-secret",
+    help="Sync API key secret",
+    prompt=True,
+    hide_input=True,
+    default=getattr(API_KEY, "secret", None),
+)
+@click.option(
+    "--aws-role-ext-id",
+    help="External ID for the AWS IAM role. Hidden option used for testing.",
+    default=lambda: str(uuid4()),
+    hidden=True,
+)
 def create_workspace_config(
     workspace_id: str,
+    databricks_host: str,
+    databricks_token: str,
+    sync_api_key_id: str,
+    sync_api_key_secret: str,
+    aws_role_ext_id: str,
+    databricks_plan_type: DatabricksPlanType,
+    hosting_type: HostingType,
     monitoring_type: WorkspaceMonitoringTypeEnum,
-    instance_profile_arn: str = None,
-    databricks_plan_type: str = None,
-    databricks_webhook_id: str = None,
-    aws_region: str = None,
-    cluster_policy_id: str = None,
-    hosting_type: HostingType = None,
-    compute_provider: ComputeProvider = None,
-    aws_iam_role_arn: str = None,
-    azure_subscription_id: str = None,
-    azure_client_id: str = None,
-    azure_tenant_id: str = None,
-    azure_client_secret: str = None,
+    instance_profile_arn: Optional[str] = None,
+    databricks_webhook_id: Optional[str] = None,
+    aws_region: Optional[str] = None,
+    cluster_policy_id: Optional[str] = None,
+    compute_provider: Optional[ComputeProvider] = None,
+    aws_iam_role_arn: Optional[str] = None,
+    azure_subscription_id: Optional[str] = None,
+    azure_client_id: Optional[str] = None,
+    azure_tenant_id: Optional[str] = None,
+    azure_client_secret: Optional[str] = None,
 ):
-    databricks_host, databricks_token = _prompt_databricks_fields()
-
-    sync_api_key_id, sync_api_key_secret = _prompt_sync_api_fields()
-
-    compute_provider: ComputeProvider = _determine_compute_provider(
-        compute_provider, databricks_host
-    )
+    compute_provider = _determine_compute_provider(compute_provider, databricks_host)
 
     aws_region = _prompt_aws_region(aws_region, compute_provider)
+    if compute_provider == ComputeProvider.AWS and not aws_region:
+        raise click.BadParameter("AWS region is required for when using AWS")
 
-    hosting_type = _prompt_hosting_type(hosting_type)
     collection_type = _determine_collection_type(hosting_type)
-    monitoring_type = _determine_monitoring_type(collection_type, monitoring_type)
 
-    aws_external_id = str(uuid4())
-
-    if hosting_type == HostingType.SELF_HOSTED and compute_provider == ComputeProvider.AWS:
-        instance_profile_arn = instance_profile_arn or click.prompt("AWS Instance profile ARN")
+    if (
+        hosting_type == HostingType.SELF_HOSTED
+        and compute_provider == ComputeProvider.AWS
+    ):
+        instance_profile_arn = instance_profile_arn or click.prompt(
+            "AWS Instance profile ARN"
+        )
 
     if hosting_type == HostingType.SYNC_HOSTED:
         if compute_provider == ComputeProvider.AWS:
-            aws_iam_role_arn = _prompt_aws_arn_info(aws_external_id, aws_iam_role_arn)
+            aws_iam_role_arn = _prompt_aws_arn_info(
+                aws_external_id=aws_role_ext_id, aws_iam_role_arn=aws_iam_role_arn
+            )
         elif compute_provider == ComputeProvider.AZURE:
             (
                 azure_client_id,
@@ -136,7 +167,10 @@ def create_workspace_config(
                 azure_subscription_id,
                 azure_tenant_id,
             ) = _handle_azure_provider(
-                azure_client_id, azure_client_secret, azure_subscription_id, azure_tenant_id
+                azure_client_id,
+                azure_client_secret,
+                azure_subscription_id,
+                azure_tenant_id,
             )
 
     workspace_config = CreateWorkspaceConfig(
@@ -146,7 +180,9 @@ def create_workspace_config(
         sync_api_key_id=sync_api_key_id,
         sync_api_key_secret=sync_api_key_secret,
         collection_type=collection_type,
-        monitoring_type=monitoring_type,
+        monitoring_type=None
+        if collection_type == WorkspaceCollectionTypeEnum.REMOTE
+        else monitoring_type,
         compute_provider=compute_provider,
         instance_profile_arn=instance_profile_arn,
         databricks_plan_type=databricks_plan_type,
@@ -154,7 +190,7 @@ def create_workspace_config(
         aws_region=aws_region,
         aws_iam_role_arn=aws_iam_role_arn,
         cluster_policy_id=cluster_policy_id,
-        external_id=aws_external_id,
+        external_id=aws_role_ext_id,
         azure_tenant_id=azure_tenant_id,
         azure_client_id=azure_client_id,
         azure_client_secret=azure_client_secret,
@@ -164,16 +200,21 @@ def create_workspace_config(
 
     config = response.result
     if config:
-        click.echo(json.dumps(config, indent=2, cls=DateTimeEncoderNaiveUTCDropMicroseconds))
+        click.echo(
+            json.dumps(config, indent=2, cls=DateTimeEncoderNaiveUTCDropMicroseconds)
+        )
     else:
         click.echo(str(response.error), err=True)
+        raise click.Abort("Failed to create workspace configuration")
 
 
-def _prompt_aws_region(aws_region, compute_provider):
+def _prompt_aws_region(
+    aws_region: str | None, compute_provider: ComputeProvider
+) -> str | None:
     if compute_provider == ComputeProvider.AWS:
         aws_region = aws_region or click.prompt(
             "AWS Region (e.g. us-east-1)",
-            type=click.Choice(AwsRegionEnum),
+            type=click.Choice(list(AwsRegionEnum)),
             show_choices=False,
             default=AwsRegionEnum.US_EAST_1,
             show_default=True,
@@ -181,50 +222,16 @@ def _prompt_aws_region(aws_region, compute_provider):
     return aws_region
 
 
-def _prompt_aws_arn_info(aws_external_id, aws_iam_role_arn):
+def _prompt_aws_arn_info(aws_external_id: str, aws_iam_role_arn: Optional[str]) -> str:
     instructions = AwsHostedIAMInstructions(external_id=aws_external_id)
     click.echo(click.style(text=instructions.step_1_prompt, bold=True))
     click.echo(instructions.step_1_value)
     click.echo(click.style(text=instructions.step_2_prompt, bold=True))
     click.echo(instructions.step_2_value)
-    aws_iam_role_arn = aws_iam_role_arn or click.prompt(
+    role_arn = aws_iam_role_arn or click.prompt(
         "AWS IAM Role ARN",
     )
-    return aws_iam_role_arn
-
-
-def _prompt_hosting_type(hosting_type):
-    hosting_type = hosting_type or click.prompt(
-        "Hosting type (sync-hosted or self-hosted)",
-        type=click.Choice([h.value for h in HostingType]),
-        default=HostingType.SYNC_HOSTED,
-        show_default=True,
-    )
-    return hosting_type
-
-
-def _prompt_sync_api_fields():
-    sync_api_key_id = click.prompt("Sync API key ID", default=getattr(API_KEY, "id", None))
-    sync_api_key_secret = click.prompt(
-        "Sync API key secret",
-        default=getattr(API_KEY, "secret", None),
-        hide_input=True,
-        show_default=False,
-    )
-    return sync_api_key_id, sync_api_key_secret
-
-
-def _prompt_databricks_fields():
-    databricks_host = click.prompt(
-        "Databricks host (prefix with https://)", default=getattr(DB_CONFIG, "host", None)
-    )
-    databricks_token = click.prompt(
-        "Databricks token",
-        default=getattr(DB_CONFIG, "token", None),
-        hide_input=True,
-        show_default=False,
-    )
-    return databricks_host, databricks_token
+    return role_arn
 
 
 def _determine_collection_type(hosting_type) -> WorkspaceCollectionTypeEnum:
@@ -235,19 +242,12 @@ def _determine_collection_type(hosting_type) -> WorkspaceCollectionTypeEnum:
     )
 
 
-def _determine_monitoring_type(
-    collection_type: WorkspaceCollectionTypeEnum, monitoring_type: WorkspaceMonitoringTypeEnum
-) -> Optional[WorkspaceMonitoringTypeEnum]:
-    if collection_type == WorkspaceCollectionTypeEnum.REMOTE:
-        return None
-
-    return monitoring_type
-
-
 def _handle_azure_provider(
     azure_client_id, azure_client_secret, azure_subscription_id, azure_tenant_id
 ):
-    azure_subscription_id = azure_subscription_id or click.prompt("Azure Subscription ID")
+    azure_subscription_id = azure_subscription_id or click.prompt(
+        "Azure Subscription ID"
+    )
     azure_client_id = azure_client_id or click.prompt("Azure Client ID")
     azure_tenant_id = azure_tenant_id or click.prompt("Azure Tenant ID")
     azure_client_secret = azure_client_secret or click.prompt(
@@ -256,7 +256,7 @@ def _handle_azure_provider(
     return azure_client_id, azure_client_secret, azure_subscription_id, azure_tenant_id
 
 
-def _get_provider_from_url(url: str) -> str:
+def _get_provider_from_url(url: str) -> Optional[ComputeProvider]:
     netloc = urlparse(url).netloc
     if netloc.endswith(".azuredatabricks.net"):
         return ComputeProvider.AZURE
@@ -264,13 +264,15 @@ def _get_provider_from_url(url: str) -> str:
         return ComputeProvider.AWS
 
 
-def _determine_compute_provider(compute_provider, databricks_host):
+def _determine_compute_provider(
+    compute_provider: Optional[ComputeProvider], databricks_host: str
+) -> ComputeProvider:
     return (
         compute_provider
         or _get_provider_from_url(databricks_host)
         or click.prompt(
             "Compute provider (aws or azure)",
-            type=click.Choice(ComputeProvider),
+            type=click.Choice(list(ComputeProvider)),
             default=ComputeProvider.AWS,
             show_default=True,
         )
@@ -353,43 +355,63 @@ def list_workspace_configs():
     type=click.Choice(WorkspaceMonitoringTypeEnum),
     help="Choose how you want Gradient to monitor your Databricks clusters.",
 )
+@click.option(
+    "--databricks-host",
+    help="Databricks host (prefix with https://)",
+    default=OPTIONAL_DEFAULT,
+    show_default=False,
+)
+@click.option(
+    "--databricks-token",
+    help="Databricks token",
+    default=OPTIONAL_DEFAULT,
+    show_default=False,
+)
+@click.option(
+    "--sync-api-key-id",
+    help="Sync API key ID",
+    default=OPTIONAL_DEFAULT,
+    show_default=False,
+)
+@click.option(
+    "--sync-api-key-secret",
+    help="Sync API key secret",
+    default=OPTIONAL_DEFAULT,
+    show_default=False,
+)
 def update_workspace_config(
     workspace_id: str,
-    instance_profile_arn: str = None,
-    databricks_plan_type: str = None,
-    databricks_webhook_id: str = None,
-    aws_region: str = None,
-    cluster_policy_id: str = None,
-    aws_iam_role_arn: str = None,
-    azure_subscription_id: str = None,
-    azure_client_id: str = None,
-    azure_tenant_id: str = None,
-    azure_client_secret: str = None,
-    collection_type: WorkspaceCollectionTypeEnum = None,
-    monitoring_type: WorkspaceMonitoringTypeEnum = None,
+    databricks_host: str,
+    databricks_token: str,
+    sync_api_key_id: str,
+    sync_api_key_secret: str,
+    instance_profile_arn: Optional[str] = None,
+    databricks_plan_type: Optional[str] = None,
+    databricks_webhook_id: Optional[str] = None,
+    aws_region: Optional[str] = None,
+    cluster_policy_id: Optional[str] = None,
+    aws_iam_role_arn: Optional[str] = None,
+    azure_subscription_id: Optional[str] = None,
+    azure_client_id: Optional[str] = None,
+    azure_tenant_id: Optional[str] = None,
+    azure_client_secret: Optional[str] = None,
+    collection_type: Optional[WorkspaceCollectionTypeEnum] = None,
+    monitoring_type: Optional[WorkspaceMonitoringTypeEnum] = None,
 ):
     current_config_response = workspace.get_workspace_config(workspace_id)
     current_config = current_config_response.result
     if current_config:
-        databricks_host = click.prompt(
-            "Databricks host (prefix with https://, optional)",
-            default=OPTIONAL_DEFAULT,
-            show_default=False,
-        )
-        databricks_token = click.prompt(
-            "Databricks token (optional)", default=OPTIONAL_DEFAULT, show_default=False
-        )
-        sync_api_key_id = click.prompt(
-            "Sync API key ID (optional)", default=OPTIONAL_DEFAULT, show_default=False
-        )
-        sync_api_key_secret = click.prompt(
-            "Sync API key secret (optional)", default=OPTIONAL_DEFAULT, show_default=False
-        )
         update_configuration = UpdateWorkspaceConfig(
             workspace_id=workspace_id,
-            databricks_host=databricks_host if databricks_host != OPTIONAL_DEFAULT else None,
-            databricks_token=databricks_token if databricks_token != OPTIONAL_DEFAULT else None,
-            sync_api_key_id=sync_api_key_id if sync_api_key_id != OPTIONAL_DEFAULT else None,
+            databricks_host=databricks_host
+            if databricks_host != OPTIONAL_DEFAULT
+            else None,
+            databricks_token=databricks_token
+            if databricks_token != OPTIONAL_DEFAULT
+            else None,
+            sync_api_key_id=sync_api_key_id
+            if sync_api_key_id != OPTIONAL_DEFAULT
+            else None,
             sync_api_key_secret=(
                 sync_api_key_secret if sync_api_key_secret != OPTIONAL_DEFAULT else None
             ),
@@ -422,8 +444,10 @@ def update_workspace_config(
                 f"Failed to update workspace configuration. {update_config_response.error}",
                 err=True,
             )
+            raise click.Abort("Failed to update workspace configuration")
     else:
         click.echo(str(current_config_response.error), err=True)
+        raise click.Abort("Failed to update workspace configuration")
 
 
 @workspaces.command
